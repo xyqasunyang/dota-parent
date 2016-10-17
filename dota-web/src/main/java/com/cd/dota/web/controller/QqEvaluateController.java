b973a20ba7af97bbb77e7110aab652998aa816d2
suny <309592026@qq.com>
2016-10-17 18:00:12 +0800

    test

---
+++ dota-web/src/main/java/com/cd/dota/web/controller/QqEvaluateController.java
@@ -0,0 +1,63 @@
+package com.cd.dota.web.controller;
+
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cd.dota.common.WebClient;
+import com.cd.dota.core.service.RedisService;
+
+@Controller
+@RequestMapping("/qq")
+public class QqEvaluateController {
+
+	Logger logger = Logger.getLogger(QqEvaluateController.class);
+	@Autowired
+	RedisService redisService;
+	
+	
+	@RequestMapping("/evaluate.do")
+	@ResponseBody
+	public String evaluate(String qq) {
+		try {
+			String qqResult = redisService.get(qq);
+			if(qqResult!=null){
+				return "";
+			}
+			Properties p = new Properties();
+			p.load(getClass().getResourceAsStream("/appkey.properties"));
+			String key = (String) p.get("qq");
+			HashMap<String, Object> param = new HashMap<String, Object>();
+			param.put("key", key);
+			param.put("qq", qq);
+			String jsonResult = WebClient.SendGet("http://japi.juhe.cn/qqevaluate/qq", param);
+			JSONObject json = new JSONObject(jsonResult);
+			//返回状态码正确时
+			if(json.getString("error_code").equals("0")){
+				JSONObject result = json.getJSONObject("result");
+				JSONObject data = result.getJSONObject("data");
+				String conclusion = data.getString("conclusion");
+				String analysis = data.getString("analysis");
+				System.out.println("conclusion:"+conclusion+",analysis:"+analysis);
+				redisService.set(qq, conclusion+"|"+analysis);
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "dota";
+	}
+	
+	
+	public static void main(String[] args) {
+		QqEvaluateController test = new QqEvaluateController();
+		test.evaluate("309592026");
+	}
+}
---
+++ dota-web/src/main/java/com/cd/dota/web/controller/TodayController.java
@@ -0,0 +1,70 @@
+package com.cd.dota.web.controller;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Properties;
+
+import org.apache.log4j.Logger;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cd.dota.common.WebClient;
+import com.cd.dota.core.service.RedisService;
+
+@Controller
+@RequestMapping("/qq")
+public class TodayController {
+
+	Logger logger = Logger.getLogger(TodayController.class);
+	@Autowired
+	RedisService redisService;
+
+	@RequestMapping("/today.do")
+	@ResponseBody
+	public String today(String qq) {
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+			String time = sdf.format(new Date());
+			String todayResult = redisService.get(time);
+			if (todayResult != null) {
+				return "";
+			}
+			Properties p = new Properties();
+			p.load(getClass().getResourceAsStream("/appkey.properties"));
+			String key = (String) p.get("today");
+			HashMap<String, Object> param = new HashMap<String, Object>();
+			Date today = new Date();
+			param.put("key", key);
+			param.put("v", "1.0");
+			param.put("month", today.getMonth());
+			param.put("day", today.getDate());
+			String jsonResult = WebClient.SendGet("http://api.juheapi.com/japi/toh", param);
+			JSONObject json = new JSONObject(jsonResult);
+			// 返回状态码正确时
+			if (json.getString("error_code").equals("0")) {
+				JSONArray result = json.getJSONArray("result");
+				String str = "";
+				for (int i = 0; i < result.length(); i++) {
+					String des = result.getJSONObject(i).getString("des");
+					String pic = result.getJSONObject(i).getString("pic");
+					str = des + "|" + pic + "||" + str;
+				}
+				redisService.set(time, str);
+			}
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "dota";
+	}
+
+//	public static void main(String[] args) {
+//		TodayController test = new TodayController();
+//		test.dotay("309592026");
+//	}
+}
